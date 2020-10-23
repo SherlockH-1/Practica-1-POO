97b1d42012d4f22e4e650f0455eab693cb228547
Evelyn135 <eveeveeve116@gmail.com>
2020-10-22 22:52:09 -0500

    se añadieron comentarios

--- practica-1.cpp
+++ practica-1.cpp
@@ -5,9 +5,13 @@ using namespace std;
 
 int main(){
     setlocale(LC_ALL,"");
+
+//Declaración de variables
     int producto, cantidad;
     float subtotal, total;
     char otra;
+
+//Se muestra el menú de opciones para que el usuario haga una elección
     cout <<"Productos de almacen."<<endl;
     cout<< "\n -----MENU-----";
     cout<< "\n Producto 1 : $2.98";
@@ -17,10 +21,13 @@ int main(){
     cout<< "\n Producto 5 : $6.87 \n";
 
 
-
+//Primero se le pide al usuario que ingrese el número del producto que desea
     do {
         cout<< "\n Elija el número de producto: \n";
         cin>> producto;
+
+/*Si coincide con alguna de las opciones ahora se le pedira la cantidad del producto que quiere, se multiplica el
+precio de ese producto por la cantidad para obtener el subtotal el cual se muestra inmediatamente*/
          switch (producto) {
             case 1: cout << "\n Cantidad del producto: \n"; cin>> cantidad;
             subtotal=2.98*cantidad;
@@ -42,13 +49,21 @@ int main(){
             subtotal=6.87*cantidad;
              cout << "El subtotal es: \n "<<subtotal<<endl;
             break;
+
+/*Si la el número introducido no coincide se muestra el mensaje que no es valido entonces se ejecuta la siguiente
+instrucción*/
             default: cout<<"\n Opción no valida \n";
-        }
+            }
+
+ /*En la variable total se va guardando el subtotal de los productos elegidos, se le pregunta al usuario si desea
+ realizar otra compra, si es asi se repite el ciclo*/
     total=subtotal+total;
-     cout <<" ¿Desea realiar otra compra? \n s=si  n=no \n"; cin >>otra;
+     cout <<" ¿Desea realizar otra compra? \n s=si  n=no \n"; cin >>otra;
     }
     while ( otra=='s');
 
+//Si ya no desea realizar otra compra se muestra el valor total
     cout<< "El total es: \n $"<<total<<endl;
 
+    return 0;
 }
