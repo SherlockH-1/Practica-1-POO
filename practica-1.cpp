2a0c0f7499712e127f651656979deb7cb4317ee1
SherlockH-1 <68795079+SherlockH-1@users.noreply.github.com>
2020-10-23 02:34:16 -0500

    Update practica-1.cpp

--- practica-1.cpp
+++ practica-1.cpp
@@ -1,52 +1,66 @@
 #include <iostream>
-
+#include <clocale> //Biblioteca para la configuración regional
+#include <cctype>
+using namespace std;
 
 int main(){
-int producto, cantidad;
-float subtotal, total;
-char otra;
-cout <<"Productos de almacen."<<endl;
-cout<< "\n ----MENU----";
-cout<< "\n Producto 1 : $2.98";
-cout<< "\n Producto 2 : $4.50";
-cout<< "\n Producto 3 : $9.98";
-cout<< "\n Producto 4 : $4.49";
-cout<< "\n Producto 5 : $6.87 \n";
+    setlocale(LC_ALL,""); //Establece la configuración regional
 
+//Declaración de variables
+    int producto{};
+    int cantidad{};
+    float subtotal;
+    float total{};
+    char otra{};
 
+//Se muestra el menú de opciones para que el usuario haga una elección
+    cout << "PRODUCTOS DE ALMACÉN.\n"
+    "\n -------MENU-------"
+    "\n Producto 1 : $2.98"
+    "\n Producto 2 : $4.50"
+    "\n Producto 3 : $9.98"
+    "\n Producto 4 : $4.49"
+    "\n Producto 5 : $6.87 \n" << endl;
 
-do {
-        cout<< "\n Elija el número de producto: \n";
-cin>> producto;
- switch (producto) {
-    case 1: cout << "\n Cantidad del producto: \n"; cin>> cantidad;
-    subtotal=2.98*cantidad;
-    cout << "El subtotal es: \n $"<<subtotal<<endl;
-    break;
-    case 2: cout << "\n Cantidad del producto: \n"; cin>> cantidad;
-    subtotal=4.50*cantidad;
-     cout << "El subtotal es: \n $"<<subtotal<<endl;
-    break;
-    case 3: cout << "\n Cantidad del producto: \n"; cin>> cantidad;
-    subtotal=9.98*cantidad;
-     cout << "El subtotal es: \n $"<<subtotal<<endl;
-    break;
-    case 4: cout << "\n Cantidad del producto: \n"; cin>> cantidad;
-    subtotal=4.49*cantidad;
-     cout << "El subtotal es: /n $"<<subtotal<<endl;
-    break;
-    case 5: cout << "\n Cantidad del producto: \n"; cin>> cantidad;
-    subtotal=6.87*cantidad;
-     cout << "El subtotal es: /n "<<subtotal<<endl;
-    break;
-    default: cout<<"\n Opción no valida \n";
+//Primero se le pide al usuario que ingrese el número del producto que desea
+    do {
+        cout << "Escriba el número de producto: ";
+        cin >> producto;
+/*Si coincide con alguna de las opciones, se le pedira la cantidad del producto que quiere. Se multiplica el
+precio de ese producto por la cantidad para obtener el subtotal el cual se muestra inmediatamente*/
+    switch (producto) {
+        case 1: cout << "Cantidad del producto: "; cin >> cantidad;
+                subtotal= 2.98 * cantidad;
+                cout << "El subtotal es: $" << subtotal << endl;
+            break;
+        case 2: cout << "Cantidad del producto: "; cin >> cantidad;
+                subtotal= 4.50 * cantidad;
+                cout << "El subtotal es: $" << subtotal << endl;
+            break;
+        case 3: cout << "Cantidad del producto: "; cin >> cantidad;
+                subtotal = 9.98 * cantidad;
+                cout << "El subtotal es: $" <<subtotal << endl;
+            break;
+        case 4: cout << "Cantidad del producto: "; cin >> cantidad;
+                subtotal= 4.49 * cantidad;
+                cout << "El subtotal es: $" << subtotal << endl;
+            break;
+        case 5: cout << "Cantidad del producto: "; cin >> cantidad;
+                subtotal= 6.87 * cantidad;
+                cout << "El subtotal es: $" <<subtotal<<endl;
+            break;
+/*Si el número introducido no coincide, alerta que la opciónno es valida, entonces, se ejecuta la siguiente
+instrucción*/
+        default: cout << "No existe el producto." << endl;
 }
-total=subtotal+total;
- cout <<"¿Desea realiar otra compra? \n s=si \n"; cin >>otra;
- cout<< "El total es: \n $"<<total<<endl;
+ /*En la variable total se va guardando el subtotal de los productos elegidos; se le pregunta al usuario si desea
+ realizar otra compra, si es así, se repite el ciclo*/
+total = subtotal + total;
+ cout << "\n¿Desea realizar otra compra? (s=sí / n=no) \n"; cin >> otra;
 }
-while ( otra=='s');
-
-cout<< "El total es: \n $"<<total<<endl;
-
+    while ( tolower(otra) == 's');
+//Si ya no desea realizar otra compra, se muestra el valor total
+    cout << "El total es: $" << total << endl;
+    return 0;
 }
+
